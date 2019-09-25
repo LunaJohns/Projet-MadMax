3d80f8da59266dcfdf8675f5fe7ed46df04c3fbb
abdelrahmane <abdelrahmane@10.192.128.248>
2019-09-25 09:15:30 +0200

    Decryptage - trouver la cle pour le décryptage du XOR

--- MAD MAX/src/controleur/Controleur.java
+++ MAD MAX/src/controleur/Controleur.java
@@ -9,12 +9,18 @@ public class Controleur
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		CAD test = new CAD();
-		test.Connexion("SELECT id, Identifiant, Mdp FROM utilisateurs;");
+		//CAD test = new CAD();
+		//test.Connexion("SELECT id, Identifiant, Mdp FROM utilisateurs;");
 		
 		Decryptage test2 = new Decryptage();
 		test2.genererListeCle(0);
-
+		
+		//Binaire
+		int[] t1 = {122,122,122,122};
+		//String resultat1 = test2.decrypterBinaire(t1, "00010101000001000001001000010101"); 
+		//Caractère
+		String resultat2 = test2.decrypterCaractere(t1, "test");
+		System.out.println(resultat2);
 	}
 
 }
--- MAD MAX/src/model/Decryptage.java
+++ MAD MAX/src/model/Decryptage.java
@@ -3,39 +3,155 @@ package model;
 public class Decryptage 
 {
 	
-	public int[] listeCle;
+	public int[] Cle;
+	public Boolean cleTrouve = false;
 	
-
 	public Decryptage()
 	{
-		listeCle = new int[12];
+		Cle = new int[4];
 		
-		for (int i = 0; i < listeCle.length; i++) 
+		for (int i = 0; i < Cle.length; i++) 
 		{
 
-			listeCle[i] = 97;
+			Cle[i] = 97;
 		}
 		
 	}
 	
 	public void genererListeCle(int index)
 	{
-		if (index < listeCle.length)
+		if (index < Cle.length && cleTrouve != true)
 		{
 			for (int x = 97; x <= 122;x++)
 			{
 				
-				listeCle[index] = x;
-				toString(listeCle);
-				genererListeCle(index + 1);
+				Cle[index] = x;
 				
+				String chaineDecrypter = decrypterCaractere(Cle,"	");
+				if (chaineDecrypter.equals("test"))
+				{
+					System.out.print("Clé trouvé : ");
+					cleTrouve = true;
+					toString(Cle);
+					
+				}
+				genererListeCle(index + 1);
 				
 			}
 		}
-		
 
 	}
 	
+	public String decrypterBinaire(int [] CleDecimal, String chaineCrypter)
+	{
+		int i = 0;
+		int j = 0;
+		
+		int decoupageBits = 0;
+		int tailleChaineCrypter = chaineCrypter.length()/8;
+		
+		//Stockage du message en binaire
+		String[] chaineCrypterBinaire = new String[tailleChaineCrypter];
+		
+		//Stockage du message en Decimal
+		int[] chaineCrypterDecimal = new int[tailleChaineCrypter];
+		
+		//Decoupage du message binaire par paquet de 8 bits
+		while (i < tailleChaineCrypter)
+		{
+			chaineCrypterBinaire[i]  = chaineCrypter.substring(decoupageBits,decoupageBits+8);
+			i++;
+			decoupageBits += 8;
+		}
+		
+		//Conversion - binaire -> décimal
+		for (i = 0; i < chaineCrypterBinaire.length; i++) 
+		{
+			chaineCrypterDecimal[i] = Integer.parseInt(chaineCrypterBinaire[i], 2);
+		}
+
+		//Decryptage
+		//Tableau de int permettant de stocker le message Decrypter (en décimal)
+		int[] chaineDecrypterDecimal = new int[chaineCrypter.length()];
+		
+		for (i = 0; i < chaineCrypterDecimal.length; i++) 
+		{
+			//On test si on doit reprendre la clé au début (dans le cas ou le message est plus long que la clé)
+			if (j == CleDecimal.length)
+			{
+				j = 0;
+			}
+			//Opération XOR
+			chaineDecrypterDecimal[i] = CleDecimal[j]^chaineCrypterDecimal[i];
+			j++;
+		}
+		
+		//Conversion - Decimal -> Text
+		char [] chaineDecrypterText = new char[chaineCrypter.length()];
+		
+		for (i = 0; i < tailleChaineCrypter; i++) 
+		{
+			chaineDecrypterText[i] = (char) chaineDecrypterDecimal[i];
+		}
+		
+		//Affichage
+		
+		//Afficher la Clé en décimal
+		System.out.print("Cle :");
+		for (i = 0; i < CleDecimal.length; i++) 
+		{
+			System.out.print(" " + CleDecimal[i]);
+		}
+		
+		//Afficher la chaine - binaire
+		System.out.print("\nMessage en binaire : ");
+		for (i = 0; i < chaineCrypterBinaire.length; i++) 
+		{
+			System.out.print(" " + chaineCrypterBinaire[i]);
+		}
+		
+		//Afficher la chaine - décimal
+		System.out.print("\nMessage en décimal : ");
+		for (i = 0; i < chaineCrypterDecimal.length; i++) 
+		{
+			System.out.print(" " + chaineCrypterDecimal[i]);
+		}
+		
+		//Afficher la chaine Decrypter
+		System.out.print("\nChaine Decryptée en décimal :");
+		for (i = 0; i < tailleChaineCrypter; i++) 
+		{
+			System.out.print(" " + chaineDecrypterDecimal[i]);
+		}
+		
+		//Afficher la chaine Decrypter 
+		System.out.print("\nChaine Decryptée : ");
+		for (i = 0; i < tailleChaineCrypter; i++) 
+		{
+			System.out.print(chaineDecrypterText[i]);
+		}
+		
+		//Conversion tableau de char en String
+		String chaineDecrypter = "";
+		for (i = 0;i < tailleChaineCrypter;i++)
+		{
+			chaineDecrypter = new StringBuffer().append(chaineDecrypter).append(chaineDecrypterText[i]).toString();
+		}
+		return chaineDecrypter;
+		
+	}
+	
+	public String decrypterCaractere(int [] cleDecimal,String chaineCrypter) {
+		String chaineDecrypter = "";
+		char tmp;
+		for(int i = 0; i < chaineCrypter.length(); i++) {
+			//On execcute le XOR avec key.i et toDecrypt.i
+			tmp = (char) (cleDecimal[i%cleDecimal.length] ^ chaineCrypter.charAt(i));
+			chaineDecrypter = new StringBuffer().append(chaineDecrypter).append(tmp).toString(); 
+		}
+		//System.out.println("\nMessage décrypté (méthode caractère) : "+ chaineDecrypter);
+		return chaineDecrypter;
+	}
 	
 	public void toString(int[] tab) 
 	{
